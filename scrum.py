d41cd014e5e9f35b7e3d6350ae43f383be4dbc4c
Drew Leonard <andy.j.leonard@gmail.com>
2018-05-03 02:45:19 +0100

    Basic scrum-triggering function

--- scrum.py
+++ scrum.py
@@ -5,10 +5,13 @@ async def trigger_scrum(opsdroid, config, message):
     """Begin the scrum process for the specified users"""
 
     # Identify users to scrum
+    users = message.text.split()[1:]
 
-    # Reset any previous scrum info
+    # Overwrite any previous scrum info with blank structure for each user
+    opsdroid.memory.put('scrum_info', {user: {} for user in users})
 
     # Create and join rooms with those users if not in one already, to listen for scrumming
+    # Pending
 
 
 async def report_scrum(opsdroid, config, message):
