9a5fa7eef9ce67f0eb89164a514b35afe689e77b
Drew Leonard <andy.j.leonard@gmail.com>
2018-05-03 17:21:26 +0100

    Assorted bug-squashing

--- scrum.py
+++ scrum.py
@@ -8,26 +8,29 @@ async def trigger_scrum(opsdroid, config, message):
     users = message.text.split()[1:]
 
     # Overwrite any previous scrum info with blank structure for each user
-    opsdroid.memory.put('scrum_info', {user: {} for user in users})
+    await opsdroid.memory.put('scrum_info', {user: {} for user in users})
 
     # Create and join rooms with those users if not in one already, to listen for scrumming
     # Pending
 
 
+@match_regex("!yesterday")
+@match_regex("!today")
+@match_regex("!blockers")
 async def report_scrum(opsdroid, config, message):
     """Get scrum input from users and report back into the main room when they've said all three things"""
 
-    scrum_info = opsdroid.memory.get('scrum_info')
+    scrum_info = await opsdroid.memory.get('scrum_info')
     if message.user not in scrum_info.keys():
         return
 
     for command in ["!yesterday", "!today", "!blockers"]:
         if command in message.text:
             # Strip the command bit from the message
-            report = message.replace(command, '')
+            report = message.text.replace(command, '')
             # Add report to memory
             scrum_info[message.user][command] = report
-            opsdroid.memory.put('scrum_info', scrum_info)
+            await opsdroid.memory.put('scrum_info', scrum_info)
 
     # If all three things have been reported, sent that info back to the main room
     reports = scrum_info[message.user]
